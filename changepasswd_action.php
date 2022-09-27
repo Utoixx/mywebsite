ac7b608dba698e802e1c36a4fd4d829b69a6bea5
Ununtu <quoctuan2000bg@gmail.com>
2022-09-27 20:57:30 +0700

    Zero code

--- changepasswd_action.php
+++ changepasswd_action.php
@@ -33,7 +33,6 @@
     }else{
         $message = "Current password incorrect!";
         echo "<script type='text/javascript'>alert('$message');</script>";
-        //header('Location: changepasswd_form.php');
     }
     function checkuser()
     {
